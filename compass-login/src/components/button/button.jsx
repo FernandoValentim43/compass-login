f304a2a79ca01fe83a9b4b206b145293318aa78b
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-06-24 01:10:38 -0300

    Refactor: refactoring in the button component and its styling

--- compass-login/src/components/button/button.jsx
+++ compass-login/src/components/button/button.jsx
@@ -1,28 +1,12 @@
 import React from "react";
-import { ButtonLogin, ButtonNavegate, ButtonLogOut } from "./buttonStyled";
+import { ButtonStyled } from "./buttonStyled";
 
 const Button = (props) => {
-    if(props.id=="bt1"){
-        return (
-            <div>
-                <ButtonLogin id={props.id}>Continue</ButtonLogin>
-            </div>
-        )
-    }
-    if(props.id=="bt2"){
-        return (
-            <div>
-                <ButtonNavegate id={props.id}>Continuar Navegando</ButtonNavegate>
-            </div>
-        )
-    }
-    else{
-        return (
-            <div>
-                <ButtonLogOut id={props.id}>Continue</ButtonLogOut>
-            </div>
-        )
-    }
+    return (
+        <ButtonStyled>
+            <button id={props.id} className={props.name}>{props.text}</button>
+        </ButtonStyled>
+    )
 }
 
 export default Button;
--- compass-login/src/components/button/buttonStyled.js
+++ compass-login/src/components/button/buttonStyled.js
@@ -1,35 +1,21 @@
 import styled from 'styled-components';
 
-const ButtonLogin = styled.button`
-    cursor: pointer;
-    border-radius: 3.125rem;
-    box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
-    background: linear-gradient(90deg, #FF2D04 0%, #C13216 100%);
-    border: 2px solid transparent;
-    border-image: linear-gradient(#FF2D04 100%, #C13216 100%);
-    border-image-slice: 0;
-    font-family: 'Poppins';
-    font-style: normal;
-    color: white;
-    width: 23.68rem;
-    height: 4.18rem;
-    font-size: 1.125rem;
+const ButtonStyled = styled.div`
+    .botao1{
+        cursor: pointer;
+        border-radius: 3.125rem;
+        box-shadow: inset 5px 5px 15px rgba(0, 0, 0, 0.15);
+        background: linear-gradient(90deg, #FF2D04 0%, #C13216 100%);
+        border: 2px solid transparent;
+        border-image: linear-gradient(#FF2D04 100%, #C13216 100%);
+        border-image-slice: 0;
+        font-family: 'Poppins';
+        font-style: normal;
+        color: white;
+        width: 23.68rem;
+        height: 4.18rem;
+        font-size: 1.125rem;
+    }
 `;
 
-const ButtonLogOut = styled.button`
-    cursor: pointer;
-`;
-
-const ButtonNavegate = styled.button`
-    cursor: pointer;
-    background-color: white;
-    width: 8.18rem;
-    height: 6.18rem;
-    color: #C13216;
-    font-family: 'Poppins';
-    font-style: normal;
-    font-size: 1.125rem;
-    border: none;
-`;
-
-export { ButtonLogin, ButtonLogOut, ButtonNavegate }
+export { ButtonStyled }
