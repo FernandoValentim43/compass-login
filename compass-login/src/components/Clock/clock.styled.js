0792c3d66c14c4d9fe3ca769aa5859a2722ab61b
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-06-25 02:49:41 -0300

    Style: clock component margins removed

--- compass-login/src/components/Clock/clock.styled.js
+++ compass-login/src/components/Clock/clock.styled.js
@@ -1,20 +1,19 @@
 import styled from 'styled-components';
 
 const TextClock = styled.p`
-    margin: 0 auto;
-    font-size: 100px;
+    font-size: 6.25rem;
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 700;
     margin: 0;
-    height: 117px;
+    height: 7.31rem;
 `;
 const TextDate = styled.p`
     margin: 0;
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
-    font-size: 14px;
+    font-size: 0.87rem;
     justify-self: center;
 `;
 
