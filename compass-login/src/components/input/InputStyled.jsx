664ef13a450d1b5e15b211e3b308343b0f89a88d
fernando <fernandoreal.valentim@gmail.com>
2022-06-25 18:20:40 -0300

    Fix: styledinput placing

--- compass-login/src/components/input/InputStyled.jsx
+++ compass-login/src/components/input/InputStyled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import Input from './Input'
+
 export const InputStyled = styled.div`
     
     input {
--- compass-login/src/components/input/input.jsx
+++ compass-login/src/components/input/input.jsx
@@ -1,10 +1,14 @@
+import { InputStyled } from "./InputStyled";
+
 
 export function Input (props) {
 return (
     <div className="input-div">
-        <div className="input-wrapper">
-             <input  className="Input" type={props.type} placeholder={props.placeholder}/>
-        </div>
+        <InputStyled>
+            <div className="input-wrapper">
+                <input  className="Input" type={props.type} placeholder={props.placeholder}/>
+            </div>
+        </InputStyled>
     </div>
  );
    
