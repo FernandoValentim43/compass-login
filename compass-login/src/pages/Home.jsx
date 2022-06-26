23af5dd3b3086f1658e4a59ea7f7d59317bdc3ba
palomacha100 <palomacha100@gmail.com>
2022-06-26 12:39:31 -0300

    Feat: Creating Weather component

--- compass-login/src/pages/Home.jsx
+++ compass-login/src/pages/Home.jsx
@@ -3,21 +3,19 @@ import Image from "../components/Image/Image"
 
 import LogoBall from "../assets/uolball.png"
 import LogoBlack from "../assets/compass-logo-black.png"
+import Weather from '../components/Weather/Weather'
 
 function Home() {
     return (
-    <HomeStyled>
-        <section className="main">
-        <header>
-        <Image src={LogoBlack} wrapper="logo-black" />
-        </header>
-
-        <Image src={LogoBall} wrapper="logo-ball" />
-
-
-        </section>
-    </HomeStyled>
-      
+      <HomeStyled>
+          <section className="main">
+            <header>
+              <Image src={LogoBlack} wrapper="logo-black" />
+            </header>
+            <Image src={LogoBall} wrapper="logo-ball" />
+            <Weather />
+          </section>
+      </HomeStyled> 
     )
   }
   
