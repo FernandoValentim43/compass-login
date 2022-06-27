c550d721b7c340e7a0e8020431a87ad1ba4e7226
fernando <fernandoreal.valentim@gmail.com>
2022-06-27 18:26:02 -0300

    Feeature: component timer

---
+++ compass-login/src/components/Timer/Timer.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const TimerSeconds = styled.div`
+    display: flex;
+    font-size: 48px;
+    font-family: 'Poppins';
+    font-weight: 700;
+    `
