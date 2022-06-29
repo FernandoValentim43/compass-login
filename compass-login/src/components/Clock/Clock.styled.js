da8826208b1840d8e6065e22e28dfe6ed2eca68d
Joao Augusto <joaoaugustofrezende@outlook.com>
2022-06-29 17:26:29 -0300

    .

---
+++ compass-login/src/components/Clock/Clock.styled.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const TextClock = styled.p`
+    font-size: 4.5rem;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    padding: 10px;
+    text-align: center;
+    color: #222222;
+    height: 7.31rem;
+`;
+const TextDate = styled.p`
+    margin: 0;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.87rem;
+    justify-self: center;
+   
+    padding-bottom: 35px;
+    
+`;
+
+const GeneralDiv = styled.div`
+    display: flex;
+    height: 140px;
+    min-height: 140px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    
+    
+    margin-left: 10%;
+    
+    @media(max-width: 800px) {
+        display: none;
+    }
+    
+`;
+
+export {TextClock, TextDate, GeneralDiv}
